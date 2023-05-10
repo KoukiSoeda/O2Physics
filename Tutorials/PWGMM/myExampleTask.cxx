9545287d32fb9ee746e0f22a376920ed44b17756
Koki Soeda <111231349+KoukiSoeda@users.noreply.github.com>
2023-05-10 13:45:25 +0900

    New Analysis Code

--- Tutorials/PWGMM/myExampleTask.cxx
+++ Tutorials/PWGMM/myExampleTask.cxx
@@ -13,8 +13,11 @@
 ///        it is meant to be a blank page for further developments.
 /// \author everyone
 
+#include <iostream>
+#include <cmath>
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
+#include "Common/DataModel/TrackSelectionTables.h"
 
 using namespace o2;
 using namespace o2::framework;
@@ -23,19 +26,32 @@ struct myExampleTask {
   // Histogram registry: an object to hold your histograms
   HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
+  Configurable<int> nBinsPt{"nBinsPt", 100, "N bins in pT histo"};
+
   void init(InitContext const&)
   {
     // define axes you want to use
+    const AxisSpec axisCounter{1, 0, +1, ""};
     const AxisSpec axisEta{30, -1.5, +1.5, "#eta"};
+    const AxisSpec axisPt{nBinsPt, 0, 10, "p_{T}"};
 
     // create histograms
+    histos.add("eventCounter", "eventCounter", kTH1F, {axisCounter});
     histos.add("etaHistogram", "etaHistogram", kTH1F, {axisEta});
+    histos.add("pTHistogram", "pTHistogram", kTH1F, {axisPt});
   }
 
-  void process(aod::TracksIU const& tracks)
+  void process(aod::Collision const& collision, 
+              soa::Join<aod::Tracks, aod::TracksExtra, aod::TracksDCA> const& tracks, 
+              soa::Join<aod::FwdTracks, aod::McFwdTrackLabels> const& fwdtracks, 
+              soa::Join<aod::MFTTracks, aod::McMFTTrackLabels> const& mfttracks)
   {
+    histos.fill(HIST("eventCounter"), 0.5);
     for (auto& track : tracks) {
+      if(track.tpcNClsCrossedRows() < 70) continue; //badly tracked
+      if(fabs(track.dcaXY()) > 0.2) continue;
       histos.fill(HIST("etaHistogram"), track.eta());
+      histos.fill(HIST("pTHistogram"), track.pt());
     }
   }
 };
