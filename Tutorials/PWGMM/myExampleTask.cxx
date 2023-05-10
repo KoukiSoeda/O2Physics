afd15d06fd4e423db14396d34310df839e2d9f23
Koki Soeda <111231349+KoukiSoeda@users.noreply.github.com>
2023-05-10 18:59:18 +0900

    Update 0510

--- Tutorials/PWGMM/myExampleTask.cxx
+++ Tutorials/PWGMM/myExampleTask.cxx
@@ -1,57 +1,81 @@
-// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
-//
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
-///
-/// \brief This task is an empty skeleton that fills a simple eta histogram.
-///        it is meant to be a blank page for further developments.
-/// \author everyone
+/// \author Koki Soeda
+/// \since 10/05/2023
 
 #include <iostream>
 #include <cmath>
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Common/DataModel/TrackSelectionTables.h"
+#include "TDatabasePDG.h"
+#include "MathUtils/Utils.h"
 
+using namespace std;
 using namespace o2;
 using namespace o2::framework;
 
-struct myExampleTask {
+struct MyAnalysisTask {
   // Histogram registry: an object to hold your histograms
   HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
+  Configurable<int> nBinsVer{"nBinsVer", 300, "N bins in Collisino Vertex histo"};
   Configurable<int> nBinsPt{"nBinsPt", 100, "N bins in pT histo"};
 
   void init(InitContext const&)
   {
     // define axes you want to use
+    const AxisSpec axisVertex{nBinsVer, -30, +30, "cm"};
     const AxisSpec axisCounter{1, 0, +1, ""};
+    const AxisSpec axisChi2(505, -5, +500, "");
+    const AxisSpec axisGMTpdg(2000, -1000, +1000, "");
     const AxisSpec axisEta{30, -1.5, +1.5, "#eta"};
     const AxisSpec axisPt{nBinsPt, 0, 10, "p_{T}"};
 
     // create histograms
-    histos.add("eventCounter", "eventCounter", kTH1F, {axisCounter});
-    histos.add("etaHistogram", "etaHistogram", kTH1F, {axisEta});
-    histos.add("pTHistogram", "pTHistogram", kTH1F, {axisPt});
+    histod.add("CollisionPos", "CollisionPos", kTH1F, {axisVertex});
+    histos.add("FwdParticleCounter", "FwdParticleCounter", kTH1F, {axisCounter});
+    histos.add("FwdMuonCounter", "FwdMuonCounter", kTH1F, {axisCounter});
+    histos.add("GlobalMuonTrackPDG", "GlobalMuonTrackPDG", kTH1F, {})
+    histos.add("Chi2GMT", "Chi2GMT", kTH1F, {axisChi2});
+    histos.add("MFTParticleCounter", "MFTParticleCounter", kTH1F, {axisCounter});
+    histos.add("MFTMuonCounter", "MFTMuonCounter", kTH1F, {axisCounter});
+    
   }
 
-  void process(aod::Collision const& collision, 
-              soa::Join<aod::Tracks, aod::TracksExtra, aod::TracksDCA> const& tracks, 
-              soa::Join<aod::FwdTracks, aod::McFwdTrackLabels> const& fwdtracks, 
-              soa::Join<aod::MFTTracks, aod::McMFTTrackLabels> const& mfttracks)
+  void process(soa::Join<aod::Collisions, aod::McCollisionLabels>::iterator const& collision, 
+               soa::Join<aod::FwdTracks, aod::McFwdTrackLabels> const& fwdtracks, 
+               soa::Join<aod::MFTTracks, aod::McMFTTrackLabels> const& mfttracks,
+               aod::McParticles const& MCparticle,
+               aod::McCollisions const&)
   {
-    histos.fill(HIST("eventCounter"), 0.5);
-    for (auto& track : tracks) {
-      if(track.tpcNClsCrossedRows() < 70) continue; //badly tracked
-      if(fabs(track.dcaXY()) > 0.2) continue;
-      histos.fill(HIST("etaHistogram"), track.eta());
-      histos.fill(HIST("pTHistogram"), track.pt());
+    if(collision.has_mcCollision()){
+      //Informatin of MC collision
+      auto mcCol_z = collision.mcCollison().posZ();
+      histos.fill(HIST("CollisionPos"), mcCol_z);
+
+      //Information of FwdTracks
+      for(auto& fwdtrack : fwdtracks){
+        if(!fwdtrack.has_mcParticle()) continue;
+        auto mcParticle_fwd = fwdtrack.mcParticle();
+        histos.fill(HIST("FwdParticleCounter"), 0.5);
+        if(fabs(mcParticle_fwd.pdgCode())==13) histos.fill(HIST("FwdMuonCounter"), 0.5);
+
+        if(fwdtrack.trackType()!=0) continue; //Required MFT-MCH-MID
+        auto chi2GMT = fwdtrack.chi2MatchMCHMFT();
+        auto GMTpdg = mcParticle_fwd.pdgCode();
+        histos.fill(HIST("GlobalMuonTrackPDG"), GMTpdg);
+        histos.fill(HIST("Chi2GMT"), chi2GMT);
+
+        if(chi2GMT==-1) continue;
+        if(fabs(GMTpdg)!=13) continue;
+
+      }
+      //Information of MFTTracks
+      for(auto& mfttrack : mfttracks){
+        if(!mfttrack.has_mcParticle()) continue;
+        auto mcParticle_mft = mfttrack.mcParticle();
+        histos.fill(HIST("MFTParticleCounter"), 0.5);
+        if(fabs(mcParticle_mft.pdgCode())==13) histos.fill(HIST("MFTMuonCounter"), 0.5);
+      }
     }
   }
 };
@@ -59,5 +83,5 @@ struct myExampleTask {
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<myExampleTask>(cfgc)};
+    adaptAnalysisTask<MyAnalysisTask>(cfgc)};
 }
